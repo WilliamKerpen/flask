ca01d7ed2387c7ba649ebae23198cf76a9933a63
williamkerpen <williamkerpen@hotmail.com>
2025-08-26 13:01:18 +0200

    adicionado nova rota

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for
 
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+
+@app.route('/outra_pagina')
+def nova_rota():
+    return render_template('outra_pagina.html')
