edbe160bfc4f43e739202e8c1453efc85918beec
williamkerpen <williamkerpen@hotmail.com>
2025-08-28 18:53:32 +0200

    uso de reaproveitamento de arquivos html, criacao da nav.html, contato_detail.html, com uso de rotas dinamicas.

--- app/views.py
+++ app/views.py
@@ -9,7 +9,7 @@ def homepage():
     return render_template('index.html',usuario = usuario )
 
 
-@app.route('/contato', methods= ['GET','POST'])
+@app.route('/contato/', methods= ['GET','POST'])
 def contato():
     form = ContatoForm()
     if form.validate_on_submit():
@@ -17,3 +17,22 @@ def contato():
         return redirect(url_for('homepage'))
 
     return render_template('contato.html',form=form)
+
+@app.route('/contato/lista/')
+def contatoLista():
+    if request.method == 'GET':
+        pesquisa = request.args.get ('pesquisa', '')
+
+    dados = Contato.query.order_by('nome')
+    if pesquisa != '':
+        dados = dados.filter_by(nome=pesquisa)
+
+    context = {'dados': dados.all()}
+
+    return render_template('contato_lista.html', context=context)
+
+@app.route('/contato/<int:id>/')
+def contatoDetail(id):
+    obj = Contato.query.get(id)
+
+    return render_template('contato_detail.html', obj=obj)
