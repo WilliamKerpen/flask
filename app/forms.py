99fa8f7e6dab290fa8f9d5b2c97eed37a60b9baa
williamkerpen <williamkerpen@hotmail.com>
2025-08-31 13:52:43 +0200

    Finalizacao do projeto em flask, alteracao do style para bottstrap, criacao dos posts e lista de post e comentarios, criacao do alteracoes no bd

--- app/forms.py
+++ app/forms.py
@@ -1,19 +1,68 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, SubmitField, PasswordField, FileField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
-from app import db
-from app.models import Contato
+import os
+from werkzeug.utils import secure_filename
+
+from app import db, bcrypt, app
+from app.models import Contato, User, Post, PostComentarios
+
+class UserForm(FlaskForm):
+    nome = StringField('Nome', validators=[DataRequired()])
+    sobrenome = StringField('Sobrenome', validators=[DataRequired()])
+    email = StringField('E-Mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    confirmacao_senha = PasswordField('Senha', validators=[DataRequired(), EqualTo('senha')])
+    btnSubmit = SubmitField('Cadastrar')
+
+    def validade_email(self, email):
+        if User.query.filter(email=email.data).first():
+            return ValidationError('Usuário Já cadastrado com esse E-mail!!!')
+        
+    def save(self):
+        senha = bcrypt.generate_password_hash(self.senha.data.encode('utf-8'))
+        user = User(
+            nome = self.nome.data,
+            sobrenome = self.sobrenome.data,
+            email = self.email.data,
+            senha=senha
+        )
+
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+
+class LoginForm(FlaskForm):
+    email = StringField('E-Mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    btnSubmit = SubmitField('Login')
+
+    def login(self):
+        # Recuperar o usuário do e-mail
+        user = User.query.filter_by(email=self.email.data).first()
+
+        # Verificar se a senha é valida
+        if user:
+            if bcrypt.check_password_hash(user.senha, self.senha.data.encode('utf-8')):
+                # Retorna o Usuário
+                return user
+            else:
+                raise Exception('Senha Incorreta!!!')
+        else:
+            raise Exception('Usuário não encontrado!!!')
+
 
 class ContatoForm(FlaskForm):
-    nome = StringField('Nome',validators=[DataRequired()])
-    email = StringField('E-Mail',validators=[DataRequired(), Email()])
+    nome = StringField('Nome', validators=[DataRequired()])
+    email = StringField('E-Mail', validators=[DataRequired(), Email()])
     assunto = StringField('Assunto', validators=[DataRequired()])
-    mensagem = TextAreaField('Mensagem', validators=[DataRequired()])
+    mensagem = StringField('Mensagem', validators=[DataRequired()])
     btnSubmit = SubmitField('Enviar')
 
     def save(self):
-        contato = Contato (
+        contato = Contato(
             nome = self.nome.data,
             email = self.email.data,
             assunto = self.assunto.data,
@@ -22,4 +71,44 @@ class ContatoForm(FlaskForm):
 
         db.session.add(contato)
         db.session.commit()
-    
+
+
+class PostForm(FlaskForm):
+    mensagem = StringField('Mensagem', validators=[DataRequired()])
+    imagem = FileField('Imagem', validators=[DataRequired()])
+    btnSubmit = SubmitField('Enviar')
+
+    def save(self, user_id):
+        imagem = self.imagem.data
+        nome_seguro = secure_filename(imagem.filename)
+        post = Post (
+            mensagem=self.mensagem.data,
+            user_id=user_id,
+            imagem=nome_seguro
+        )
+
+        caminho = os.path.join(
+            os.path.abspath(os.path.dirname(__file__)),# Pegar a pasta que está nosso projeto
+            app.config['UPLOAD_FILES'],# Definir a pasta que configuramos para UPLOAD
+            'post',# a pasta que está os POST
+            nome_seguro
+        )
+
+        imagem.save(caminho)
+        db.session.add(post)
+        db.session.commit()
+
+
+class PostComentarioForm(FlaskForm):
+    comentario = StringField('Comentário', validators=[DataRequired()])
+    btnSubmit = SubmitField('Enviar')
+
+    def save(self, user_id, post_id):
+        comentario = PostComentarios (
+            comentario=self.comentario.data,
+            user_id=user_id,
+            post_id=post_id
+        )
+
+        db.session.add(comentario)
+        db.session.commit()
